cd6797b89cb28a42d43a2f72445106b6ed2b4b3f
samanvisatish <samanvissatish@gmail.com>
2021-08-09 13:25:20 +0530

    flashcard quiz

--- src/components/FlashcardList.js
+++ src/components/FlashcardList.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Flashcard
- from './Flashcard';
+import Flashcard from './Flashcard';
 export default function FlashcardList({flashcards}) {
     return (
         <div className="card-grid">
